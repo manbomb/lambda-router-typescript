2a2c6e8624639beb48d3b6f29df947f7911d7c36
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2024-02-06 14:33:24 -0300

    export JSONParser middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import type { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2 } from "
 
 import ExpressLambdaServer from "./ExpressLambdaServer";
 import Router, { Route, RouteOptions } from "./Router";
+import JSONBody from "./middlewares/JSONBody";
 import Middleware from "./middlewares/Middleware";
 import RateLimiter from "./middlewares/RateLimiter";
 
@@ -15,10 +16,8 @@ export type LambdaFunctionUrlEvent = APIGatewayProxyEventV2;
 export type LambdaFunctionUrlResult = APIGatewayProxyStructuredResultV2;
 
 export {
-    ExpressLambdaServer,
-    Middleware,
-    RateLimiter,
-    Repository,
+    ExpressLambdaServer, JSONBody, Middleware,
+    RateLimiter, Repository,
     Route,
     RouteOptions,
     Types
