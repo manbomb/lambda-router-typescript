f8c2c3e05461c5206235f2d508d8de3d14dafc9f
Sergio Lucas Avilla da Silva <sergio.avilla@db1.com.br>
2024-02-27 11:54:09 -0300

    implement parsers logic to router

--- src/Router.ts
+++ src/Router.ts
@@ -1,9 +1,11 @@
-import { LambdaFunctionUrlEvent } from ".";
+import { LambdaFunctionUrlEvent, LambdaFunctionUrlResult, Parser } from ".";
+import { Promiseble } from "./Types";
 import Middleware from "./middlewares/Middleware";
 
 export default class Router {
     private routes: Route[] = [];
     private middlewares: Middleware[] = [];
+    private parsers: Parser[] = [];
 
     constructor() {}
 
@@ -11,38 +13,28 @@ export default class Router {
         this.middlewares.push(middleware);
     }
 
-    get(
-        path: string,
-        callback: (event: LambdaFunctionUrlEvent) => Promise<any>,
-        options?: RouteOptions
-    ) {
+    useMiddleware = (middleware: Middleware) => this.use(middleware); // alias
+
+    useParser(parser: Parser) {
+        this.parsers.push(parser);
+    }
+
+    get(path: string, callback: RouteCallback, options?: RouteOptions) {
         this.addRoute("GET", path, callback, options);
         return this;
     }
 
-    post(
-        path: string,
-        callback: (event: LambdaFunctionUrlEvent) => Promise<any>,
-        options?: RouteOptions
-    ) {
+    post(path: string, callback: RouteCallback, options?: RouteOptions) {
         this.addRoute("POST", path, callback, options);
         return this;
     }
 
-    put(
-        path: string,
-        callback: (event: LambdaFunctionUrlEvent) => Promise<any>,
-        options?: RouteOptions
-    ) {
+    put(path: string, callback: RouteCallback, options?: RouteOptions) {
         this.addRoute("PUT", path, callback, options);
         return this;
     }
 
-    delete(
-        path: string,
-        callback: (event: LambdaFunctionUrlEvent) => Promise<any>,
-        options?: RouteOptions
-    ) {
+    delete(path: string, callback: RouteCallback, options?: RouteOptions) {
         this.addRoute("DELETE", path, callback, options);
         return this;
     }
@@ -50,7 +42,7 @@ export default class Router {
     private addRoute(
         httpMethod: string,
         path: string,
-        callback: (event: LambdaFunctionUrlEvent) => Promise<any>,
+        callback: RouteCallback,
         options?: RouteOptions
     ) {
         this.routes.push({
@@ -101,16 +93,25 @@ export default class Router {
 
         const response = await selectedRoute.callback(eventAfterMiddlewares);
 
-        return response;
+        const responseParsed = this.parsers.reduce(
+            (finalRes, parser) => parser(finalRes),
+            response
+        );
+
+        return responseParsed;
     }
 }
 
 export interface Route {
     httpMethod: string;
     path: string;
-    callback: Function;
+    callback: RouteCallback;
     auth: boolean;
 }
+
+export type RouteCallback = (
+    event: LambdaFunctionUrlEvent
+) => Promiseble<LambdaFunctionUrlResult>;
 
 export interface RouteOptions {
     auth?: boolean;
